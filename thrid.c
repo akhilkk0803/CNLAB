27bc746a4c7f08929dc679a9d3a6319ab3bd2bb5
AKHIL K KULKARNI <97751536+akhilkk0803@users.noreply.github.com>
2024-02-17 19:29:26 +0530

    Create thrid.c

---
+++ thrid.c
@@ -0,0 +1,54 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void goBackNArq(int frames,int wz){
+    int i=1;
+    while(i<=frames){
+        for(int temp=i;temp<=i+wz && temp<=frames ;temp++){
+            printf("SENDING FRAME %d\n",temp);
+        }
+        for(int j=0;j<wz && i<=frames;j++){
+            int r=rand();
+            if(r%2){
+                printf("ACK Recived for frame %d\n",i);
+                i++;
+            }
+            else {
+                printf("ACK NOT RECEIVED for frame %d \n",i);
+                break;
+            }
+        }
+        printf("RETRANSMITTING WINDOW\n");
+    }
+}
+void selective(int frames,int wz){
+     int i=1;
+    while(i<=frames){
+        for(int temp=i;temp<=i+wz && temp<=frames ;temp++){
+            printf("SENDING FRAME %d\n",temp);
+        }
+        for(int j=0;j<wz && i<=frames;j++){
+            int r=rand();
+            if(r%2){
+                printf("ACK Recived for frame %d\n",i);
+                i++;
+            }
+            else {
+                printf("ACK NOT RECEIVED for frame %d \n",i);
+                printf("Sending Back frame %d\n",i);
+                printf("ACK  RECEIVED for frame %d\n",i);
+                i++;
+            }
+        }
+        printf("RETRANSMITTING WINDOW\n");
+    }
+}
+int main() {
+    // Write C code here
+    int frames,wz;
+    printf("Enter frames and window size");
+    scanf("%d %d",&frames,&wz);
+    goBackNArq(frames,wz);
+    selective(frames,wz);
+
+    return 0;
+}
