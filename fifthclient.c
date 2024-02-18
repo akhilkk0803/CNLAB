a5dda03740fe30267f916498301af9633ee8b390
AKHIL K KULKARNI <97751536+akhilkk0803@users.noreply.github.com>
2024-02-18 15:58:09 +0530

    Update fifthclient.c

--- fifthclient.c
+++ fifthclient.c
@@ -10,30 +10,20 @@ int main()
     int client_socket;
     struct sockaddr_in serverAddress;
     client_socket=socket(AF_INET,SOCK_STREAM,0);
-    if(client_socket==-1){
-        printf("ERROR IN SOCKET");
-        exit(0);
-    }
+ 
     
     serverAddress.sin_family=AF_INET;
     serverAddress.sin_addr.s_addr=inet_addr("127.0.0.1");
     serverAddress.sin_port=htons(PORT);
     
-    if(connect(client_socket,(struct sockaddr *)&serverAddress,sizeof(serverAddress))==-1){
-        printf("ERROR IN CONNECTION");
-        exit(0);
-    }
     printf("Enter a string");
     scanf("%s",buffer);
+    
+    connect(client_socket,(struct sockaddr *)&serverAddress,sizeof(serverAddress));
 
-    if(send(client_socket,buffer,strlen(buffer),0)==-1){
-        printf("ERROR WHILE SENDING");
-        exit(0);
-    }
-    if(recv(client_socket,buffer,BUFFER_SIZE,0)==-1){
-        printf("ERROR WHILE RECEVING");
-        exit(0);
-    }
+    send(client_socket,buffer,strlen(buffer),0);
+    
+    recv(client_socket,buffer,BUFFER_SIZE,0);
     
     printf("Received from server %s",buffer);
     return 0;
